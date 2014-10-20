2be96af61bb6981eeea612f55ae059dbb91a041a
APOORVA TYAGI <apoorva.tyagi91@gmail.com>
2014-10-20 23:17:08 +0530

    Added files for color resources

---
+++ app/src/main/java/com/edward6chan/www/guardian/SplashScreen.java
@@ -0,0 +1,14 @@
+package com.edward6chan.www.guardian;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
